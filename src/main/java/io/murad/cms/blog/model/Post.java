7083878076538f402661aff9e7e3133a400c20ae
= <=>
2020-07-18 21:07:37 +0600

    post model added

--- src/main/java/io/murad/cms/blog/model/Post.java
+++ src/main/java/io/murad/cms/blog/model/Post.java
@@ -1,5 +1,39 @@
 package io.murad.cms.blog.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "post")
 public class Post {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "post_id")
+	private Long id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "content")
+	private String content;
+	
+	@Column(name = "thumbnail")
+	private String thumbnail;
+	
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	private Category category;
+	
+	private Topic topic;
 }
